28d3b30beef9b5be8e264d3eb5482ae75f4ecfb3
Koichi Oguro <koichi@Aspire-1810T.(none)>
2013-05-02 17:44:49 -0400

    Config_parser

---
+++ config_parser.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void config_parser(char * config_file, int * puerto, int * threads, int *timeout, char ** logpath, char ** logfile)
+{
+	char *tokenPtr; //puntero para los tokens
+	char string[100]; //string donde se almacenan las lineas de configuracion
+	int i; //variable para el for
+	FILE *ficheroPtr;
+
+	if(( ficheroPtr = fopen( config_file, "r")) == NULL)
+		printf("no se pudo abrir el archivo");
+	/* Comparacion de string con cada argumento de configuracion */
+	for(i=0; i<=4;i++)
+	{
+	/* Lectura de los argumentos de configuración */
+		fscanf( ficheroPtr, "%s", string);
+
+		tokenPtr= strtok( string, "="); //se extrae el argumento hasta =    
+                if(strstr(tokenPtr,"port")) //si es port
+                {   
+                        tokenPtr = strtok(NULL," "); //se extrae la configuracion
+			sscanf(tokenPtr,"%d", puerto); //se almacena
+						
+//free(tokenPtr); //se libera la memoria
+		}
+		if(strstr(tokenPtr,"threads")) //si es threads
+		{
+			tokenPtr = strtok(NULL," ");
+			sscanf(tokenPtr,"%d", threads); //se almacena
+		}
+		if(strstr(tokenPtr,"timeout"))//si es timeout
+		{
+			tokenPtr = strtok(NULL," ");
+			sscanf(tokenPtr,"%d", timeout); //se almacena
+		}
+		if(strstr(tokenPtr,"logpath"))//si es logpath
+		{
+			tokenPtr = strtok(NULL," ");
+			*logpath = (char *) malloc(sizeof(char)*strlen(tokenPtr));
+			strcpy(*logpath, tokenPtr);
+		}
+		if(strstr(tokenPtr,"logfile"))//si es logfile
+		{
+			tokenPtr = strtok(NULL," ");
+			*logfile = (char *) malloc(sizeof(char)*strlen(tokenPtr));
+			strcpy(*logfile, tokenPtr);
+		}
+	}
+	fclose(ficheroPtr);
+}
---
+++ config_parser.h
@@ -0,0 +1,1 @@
+void config_parser (char * config_file, int * puerto, int * threads, int * timeout, char ** logpath, char ** logfile);
