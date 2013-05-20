2ca551890c0b40e0376c3f9f108230e11006f09a
Diego Ramírez <syncrhonous@gmail.com>
2013-05-20 02:31:44 -0400

    debugging sessions 4, removed hash.h, put function to utils

--- hash.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-//unsigned long hash( char * str);
-//void authentication (FILE *acl, char * user, unsigned long  pass);
-
-
-
